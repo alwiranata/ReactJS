7153ac7be7dc533faaf67797e0ff37c3fb346de6
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-08-08 18:16:34 +0700

    update

---
+++ React2/jsx.jsx
@@ -0,0 +1,32 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//    <meta charset="UTF-8">
+//    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//    <title>Document</title>
+//    <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
+//    <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+//    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+// </head>
+// <body>
+//    <div id="app"></div>
+//    <script type="text/babel">
+//     const app = document.getElementById("app")
+//     const root = ReactDOM.createRoot(app)
+//     let header = (
+//         <>
+//             <h1 className="First">Hello World</h1>
+//             <br/>
+//             {new Date().toDateString()}
+//             <br></br>
+//             <p>Paragraf</p>
+//             <form >
+//                 <label htmlFor="name">Nama</label>
+//                 <input type="text" id="name"/>
+//             </form>
+//         </>
+//     )
+//     root.render(header)
+//    </script>
+// </body>
+// </html>
