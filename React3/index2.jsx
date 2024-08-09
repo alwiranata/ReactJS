6ba1ca26dd783696e4b30790ddee2d572e20d9c8
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-08-09 11:23:43 +0700

    update

---
+++ React3/index2.jsx
@@ -0,0 +1,31 @@
+// <!-- <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
+//     <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+//     <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+//     <title>Document</title>
+// </head>
+// <body>
+//     <div id="app"></div>
+//     <script type="text/babel">
+//         const app = document.getElementById("app")
+//         const root = ReactDOM.createRoot(app)
+//         const style = {
+//             color : "gray",
+//             fontSize : "20px",
+//             backgrounColor : "gray"
+//         }
+//         let header =(
+//             <div style={style}>
+//              <h1 style={{textAlign: "center"}}>Hello World</h1>
+//              <p>Ini adalah paragraf 1</p>
+//             </div>
+//         )
+//         root.render(header)
+
+//     </script>
+// </body>
+// </html> -->
---
+++ React3/index5.jsx
@@ -0,0 +1,29 @@
+// <!-- <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
+//     <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+//     <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+//     <title>Document</title>
+// </head>
+// <body>
+//     <div id="app"></div>
+//     <script type="text/babel">
+//         const app = document.getElementById("app")
+//         const root = ReactDOM.createRoot(app)
+//         let benar = true
+//         let salah = false
+//         let header = (
+//             <>
+//                 <div>
+//                    {benar && <h1>Hello world1</h1> }
+//                    {salah &&  <h1>Hello world2</h1> }
+//                 </div>
+//             </>
+//         )
+//         root.render(header)
+//     </script>
+// </body>
+// </html> -->
