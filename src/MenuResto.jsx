e26e0e26bbaf7a3737681ab0992e97e193204cc3
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-08-16 17:46:15 +0700

    update

---
+++ src/MenuResto.jsx
@@ -0,0 +1,16 @@
+const MenuResto = () => {
+	return (
+		<>
+			<div>
+				<b>Nama Menu : </b>
+				<em>Nasi Bakar</em>
+			</div>
+			<div>
+				<b>Harga : </b>
+				<em>20.000</em>
+			</div>
+		</>
+	)
+}
+
+export default MenuResto
