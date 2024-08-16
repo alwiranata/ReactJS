10bc3c92094ea6aa6503961f8edf6020ce15b251
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-08-16 09:53:02 +0700

    install react

--- React5/src/ChildComponen.jsx
+++
@@ -1,5 +0,0 @@
-function childComponen() {
-	return <h1>Hello Child</h1>
-}
-
-export default childComponen
--- React5/src/component/DemoComp.jsx
+++
@@ -1,8 +0,0 @@
-function DemoComp(props){
-    return(
-        <>
-            <h3>Say : {props.name}</h3>
-        </>
-    )
-}
-export default DemoComp
