4a4c8f5f89bc238ff58e54b6570a9862cfbe98a2
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-08-17 18:13:23 +0700

    Add rendering and loops

--- src/MenuResto.jsx
+++ src/MenuResto.jsx
@@ -1,14 +1,29 @@
+import "./MenuResto.css"
+
 const MenuResto = () => {
+	const styleMenuItem = {
+		fontSize: "20px",
+		textAlign: "center",
+	}
+	const Menu = [
+		{name: "Ayam Bakar", price: 20000},
+		{name: "Ayam Goreng", price: 30000},
+		{name: "Ayam Kremes", price: 40000},
+	]
+
 	return (
 		<>
-			<div>
-				<b>Nama Menu : </b>
-				<em>Nasi Bakar</em>
-			</div>
-			<div>
-				<b>Harga : </b>
-				<em>20.000</em>
-			</div>
+			{Menu.map((item, i) => {
+				return (
+					<div
+						className='menu-item'
+						style={styleMenuItem}
+						key={i}
+					>
+						{item.name} :Rp.{item.price}
+					</div>
+				)
+			})}
 		</>
 	)
 }
