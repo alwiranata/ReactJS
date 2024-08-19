da1661692f2f0d2b4bcb75dbc302ee3d6c24b69e
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-08-19 20:05:07 +0700

    useState

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,10 +1,16 @@
+import {useState} from "react"
+
 const Button = () => {
+	const [counter, setCounter] = useState(0)
+	let newCounter = 0
 	function ClickHandler(name) {
-		console.log(`Hello ${name}`)
+		newCounter = counter + 1
+		setCounter(newCounter)
+		console.log(`Hello ${name} ID : ${newCounter}`)
 	}
 	return (
 		<div>
-			<button onClick={() => ClickHandler("aldo")}>Klik Here</button>
+			<button onClick={() => ClickHandler("aldo")}>Klik Here {counter}</button>
 		</div>
 	)
 }
