bed786b88518cd70f35fbe60360dbb018daf77a3
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-08-20 09:36:34 +0700

    add useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import Button from "./Button"
+import {useMemo} from "react"
+import {useState} from "react"
 
 const App = () => {
+	const [like, setLike] = useState(0)
+	const [suscriber, setSuscriber] = useState(0)
+
+	function msg() {
+		console.log("Message rendered")
+		return like < 10 ? "like kurang dari 10" : "like lebih  dari 10"
+	}
+
+	const displayLike = useMemo(() => msg(), [like])
+
 	return (
 		<div>
-			<Button />
+			<button onClick={() => setLike(like + 1)}>Like {like} </button>
+			{displayLike}
+			<button onClick={() => setSuscriber(suscriber + 1)}>
+				Suscriber {suscriber}
+			</button>
 		</div>
 	)
 }
